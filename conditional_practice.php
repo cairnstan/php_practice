ae9466ff1db1d5340eb4b6f57abc75849e9d5056
cairnstan <cairnsolsen@gmail.com>
2016-07-22 11:29:13 -0500

    Adds conditional practice

---
+++ conditional_practice.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Write your if/elseif/else statement here!
+        $myName = "Greta";
+        if ($myName === "Harry") {
+            echo "Hello Harry";
+        }
+        else {
+            echo "The condition is false";
+        }
+
+
+      ?>
+    </p>
+  </body>
+</html>
