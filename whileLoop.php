f2563a6177547f69da8e1fb977411f0d2527c837
cairnstan <cairnsolsen@gmail.com>
2016-07-26 11:39:03 -0500

    Adds Do/While syntax

--- whileLoop.php
+++ whileLoop.php
@@ -1,3 +1,4 @@
+<!-- While loop -->
 <!DOCTYPE html>
 <html>
     <head>
@@ -22,6 +23,38 @@
 		}
 	}
 	echo "<p>It took {$flipCount} flips!</p>";
+	?>
+    </body>
+</html>
+
+<!-- Do/While Loop -->
+<!DOCTYPE html>
+<html>
+    <head>
+    	<link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>More Coin Flips</title>
+	</head>
+	<body>
+	<p>We will keep flipping a coin as long as the result is heads!</p>
+	<?php
+	$flipCount = 0;
+	do {
+		$flip = rand(0,1);
+		$flipCount ++;
+		if ($flip){
+			echo "<div class=\"coin\">H</div>";
+		}
+		else {
+			echo "<div class=\"coin\">T</div>";
+		}
+	} while ($flip);
+	$verb = "were";
+	$last = "flips";
+	if ($flipCount == 1) {
+		$verb = "was";
+		$last = "flip";
+	}
+	echo "<p>There {$verb} {$flipCount} {$last}!</p>";
 	?>
     </body>
 </html>
