48c3ad5e83164a0eafdfa3bff79e6956d57dbc75
cairnstan <cairnsolsen@gmail.com>
2016-07-27 18:35:32 -0500

    Adds sorting array functions

--- introFunctions.php
+++ introFunctions.php
@@ -18,3 +18,23 @@
     ?>
     </p>
 </html>
+
+<!-- sorting arrays -->
+<html>
+    <p>
+	<?php
+	// Create an array with several elements in it,
+	// then sort it and print the joined elements to the screen
+     $cheese = array("manchego", "munster", "jarlsberg", "mozzarella", "parmesean");
+    sort($cheese);
+    print join( ", ", $cheese);
+	?>
+	</p>
+	<p>
+	<?php
+	// Reverse sort your array and print the joined elements to the screen
+	rsort($cheese);
+	print join( ", ", $cheese);
+	?>
+	</p>
+</html>
