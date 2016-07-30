edade4f5bd91a51f1af1cdebd935e0cead50ee58
cairnstan <cairnsolsen@gmail.com>
2016-07-30 13:37:37 -0500

    Adds second function to buildFunctions file.

--- buildFunctions.php
+++ buildFunctions.php
@@ -13,3 +13,21 @@
       </p>
     </body>
 </html>
+
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        function aboutMe($name, $age){
+            echo "Hello! My name is " . $name . ", and I am " . $age . " years old.";
+        }
+
+        aboutMe("Cari", 33);
+        ?>
+      </p>
+    </body>
+</html>
