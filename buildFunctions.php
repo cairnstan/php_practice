ca1259a86036f30b012ffb9aa12b81e08864f769
cairnstan <cairnsolsen@gmail.com>
2016-07-30 13:34:24 -0500

    Adds buildFunctions file

---
+++ buildFunctions.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+            function greetings($name){
+                echo "Greetings, " . $name . "!";
+            }
+            greetings("Cari Tan");
+        ?>
+      </p>
+    </body>
+</html>
