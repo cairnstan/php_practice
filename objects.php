024e0378c9aa14214bfcefd39057ef04c7849fa1
cairnstan <cairnsolsen@gmail.com>
2016-07-30 20:12:40 -0500

    Adds second example of using objects.

--- objects.php
+++ objects.php
@@ -37,3 +37,41 @@
         </p>
     </body>
 </html>
+
+//second object example
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title> Practice makes perfect! </title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <p>
+        <!-- Your code here -->
+        <?php
+            class Dog {
+              public $numLegs = 4;
+              public $name;
+
+              public function __construct($name){
+                $this->name = $name;
+              }
+
+              public function bark(){
+                return "Woof!";
+              }
+
+              public function greet(){
+                return "Hello, my name is " . $this->name . "! It's great to meet you!";
+              }
+
+            }
+            $dog1 = new Dog("Barker");
+            $dog2 = new Dog("Amigo");
+
+            echo $dog1->bark();
+            echo $dog2->greet();
+        ?>
+      </p>
+    </body>
+</html>
