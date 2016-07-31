b7773399585d1a1ba1efa6b92120656c6315cf0f
cairnstan <cairnsolsen@gmail.com>
2016-07-31 12:07:32 -0500

    Adds static class example

--- moreObjects.php
+++ moreObjects.php
@@ -58,3 +58,24 @@
     </p>
   </body>
 </html>
+
+//static instances in classes
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        class King {
+          // Modify the code on line 10...
+          public static function proclaim() {
+            echo "A kingly proclamation!";
+          }
+        }
+        // ...and call the method below!
+        King::proclaim();
+      ?>
+    </p>
+  </body>
+</html>
