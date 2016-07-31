20a12d97d9847a6df38e14ae52677d0141f825da
cairnstan <cairnsolsen@gmail.com>
2016-07-31 12:38:42 -0500

    Adds looping with associative arrays

--- moreArrays.php
+++ moreArrays.php
@@ -23,3 +23,51 @@
     </p>
   </body>
 </html>
+
+//looping with associative arrays
+<html>
+  <head>
+    <title>Iteration Nation</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $food = array('pizza', 'salad', 'burger');
+        $salad = array('lettuce' => 'with',
+                   'tomato' => 'without',
+                   'onions' => 'with');
+
+      // Looping through an array using "for".
+      // First, let's get the length of the array!
+      $length = count($food);
+
+      // Remember, arrays in PHP are zero-based:
+      for ($i = 0; $i < $length; $i++) {
+        echo $food[$i] . '<br />';
+      }
+
+      echo '<br /><br />I want my salad:<br />';
+
+      // Loop through an associative array using "foreach":
+      foreach ($salad as $ingredient=>$include) {
+        echo $include . ' ' . $ingredient . '<br />';
+      }
+
+      echo '<br /><br />';
+
+      // Create your own array here and loop
+      // through it using foreach!
+        $garden = array('garlic' => 'planted',
+        'tomatos' => 'harvested',
+        'raspberries' => 'growing');
+
+        echo 'My garden has:<br />';
+
+        foreach($garden as $crop=>$status) {
+            echo $crop . ' ' . $status . '<br />';
+        }
+
+      ?>
+    </p>
+  </body>
+</html>
