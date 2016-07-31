67a7f7709016f640f8575357617894d5a070c8de
cairnstan <cairnsolsen@gmail.com>
2016-07-31 12:14:06 -0500

    More examples coming together

--- moreObjects.php
+++ moreObjects.php
@@ -79,3 +79,28 @@
     </p>
   </body>
 </html>
+
+//combines several methods
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+      class Person {
+        public static function say() {
+         echo "Here are my thoughts!";
+        }
+      }
+        class Blogger extends Person {
+            const cats = 50;
+
+        }
+
+        Blogger::say();
+        echo Blogger::cats;
+      ?>
+    </p>
+  </body>
+</html>
