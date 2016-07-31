e3ba502f51c664b643c98335dc1d0ddc6a0f209b
cairnstan <cairnsolsen@gmail.com>
2016-07-30 20:18:08 -0500

    Adds another object example using Cat class

--- objects.php
+++ objects.php
@@ -75,3 +75,36 @@
       </p>
     </body>
 </html>
+
+//third object example
+<!DOCTYPE html>
+<html>
+    <head>
+	  <title> Challenge Time! </title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <p>
+        <?php
+          // Your code here
+          class Cat {
+            public $isAlive = true;
+            public $numLegs = 4;
+            public $name;
+
+            public function __construct($name) {
+                $this->name = $name;
+            }
+
+            public function meow(){
+                return "Meow meow";
+            }
+          }
+
+          $codeCat = new Cat("CodeCat");
+
+          echo $codeCat->meow();
+        ?>
+      </p>
+    </body>
+</html>
