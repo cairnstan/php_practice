c396998f3a481ea278ed7f63a6e90375bd33fb44
cairnstan <cairnsolsen@gmail.com>
2016-07-31 11:59:52 -0500

    Adds code for inherting and over-riding parent class elements

--- moreObjects.php
+++ moreObjects.php
@@ -31,3 +31,30 @@
     </p>
   </body>
 </html>
+
+//Inherting and over riding parent class elements
+<html>
+  <head>
+    <title>Override!</title>
+  </head>
+  <body>
+    <p>
+      <?php
+       class Vehicle {
+          public function honk() {
+            return "HONK HONK!";
+          }
+        }
+        // Add your code below!
+        class Bicycle extends Vehicle {
+            public function honk() {
+               return "Beep beep!";
+            }
+        }
+
+        $bicycle = new Bicycle();
+        echo $bicycle->honk();
+      ?>
+    </p>
+  </body>
+</html>
